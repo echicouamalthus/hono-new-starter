62809e7e3b3500654cc09217ae45dc7bcfa9937e
echicouamalthus <echicouamalthus@gmail.com>
2025-06-22 04:04:29 +0200

    commit for vercel

--- src/lib/configure-open-api.ts
+++ src/lib/configure-open-api.ts
@@ -1,5 +1,5 @@
 import type { AppOpenAPI } from "./types";
-import { Scalar } from "@scalar/hono-api-reference";
+// import { Scalar } from "@scalar/hono-api-reference";
 
 export default function configureOpenApi(app: AppOpenAPI) {
   app.doc("/doc", {
@@ -10,14 +10,14 @@ export default function configureOpenApi(app: AppOpenAPI) {
     },
   });
 
-  app.get("/reference", Scalar({
-    defaultHttpClient: {
-      targetKey: "javascript",
-      clientKey: "fetch",
-    },
-    theme: "purple",
-    layout: "modern",
-    url: "/doc",
-    pageTitle: "Awesome API",
-  }));
+//   app.get("/reference", Scalar({
+//     defaultHttpClient: {
+//       targetKey: "javascript",
+//       clientKey: "fetch",
+//     },
+//     theme: "purple",
+//     layout: "modern",
+//     url: "/doc",
+//     pageTitle: "Awesome API",
+//   }));
 }
